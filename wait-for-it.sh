707799a5cfb72acf33389754172335c8f998fa6f
unokun <unokun@gmail.com>
2021-10-05 05:48:12 +0900

    first commit

---
+++ wait-for-it.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# wait-for-it.sh
+
+set -e
+
+host="$1"
+shift
+user="$1"
+shift
+password="$1"
+shift
+cmd="$@"
+
+echo "Waiting for mysql"
+until mysql -h"$host" -u"$user" -p"$password" &> /dev/null
+do
+        >&2 echo -n "."
+        sleep 1
+done
+
+>&2 echo "MySQL is up - executing command"
+
+exec $cmd
